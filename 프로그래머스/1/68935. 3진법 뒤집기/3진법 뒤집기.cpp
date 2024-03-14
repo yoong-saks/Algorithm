8f40d773c0e929f6056c2c336c402794530241d8
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-03-14 17:52:44 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v;
+    
+    while(n!=0) {
+        v.push_back(n%3);
+        n/=3;
+    }
+    
+    for(int i = 0; i<v.size(); ++i) {
+        answer += (v[v.size()-1-i])*pow(3,i);
+    }
+    return answer;
+}
