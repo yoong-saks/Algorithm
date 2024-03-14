5ff72adc7b6d36d07e0b19bc694bf7da2a336c1e
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-03-14 17:56:54 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for(int i=0; i<commands.size(); ++i)
+    {
+        vector<int> arr;
+        for(int j=commands[i][0]-1; j<commands[i][1]; ++j)
+            arr.push_back(array[j]);
+        sort(arr.begin(),arr.end());
+        answer.push_back(arr[commands[i][2]-1]);
+    }
+    
+    return answer;
+}
