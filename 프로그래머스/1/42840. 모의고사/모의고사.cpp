d4b6ab556e7f4f4728afcf99f64e40437cdcd8d3
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-03-17 21:04:01 +0900

    [level 1] Title: 모의고사, Time: 0.12 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -10,14 +10,19 @@ vector<int> solution(vector<int> answers) {
     vector<int> supo_one {1,2,3,4,5};
     vector<int> supo_two {2,1,2,3,2,4,2,5};
     vector<int> supo_thr {3,3,1,1,2,2,4,4,5,5};
-    cout << supo_one[4] <<endl;
-    for(int i=0;i<10000; ++i) {
-        if(answers[i%5] == supo_one[i%supo_one.size()]) answer_cnt[0]=answer_cnt[0]+1;
-        if(answers[i%5] == supo_two[i%supo_two.size()]) answer_cnt[1]=answer_cnt[1]+1;
-        if(answers[i%5] == supo_thr[i%supo_thr.size()]) answer_cnt[2]=answer_cnt[2]+1;
+    for(int i=0;i<answers.size(); ++i) {
+        if(answers[i%answers.size()] == supo_one[i%supo_one.size()]) answer_cnt[0]=answer_cnt[0]+1;
+        if(answers[i%answers.size()] == supo_two[i%supo_two.size()]) answer_cnt[1]=answer_cnt[1]+1;
+        if(answers[i%answers.size()] == supo_thr[i%supo_thr.size()]) answer_cnt[2]=answer_cnt[2]+1;
     }
-    cout << answer_cnt[0] << endl;
-    cout << answer_cnt[1] << endl;
-    cout << answer_cnt[2] << endl;
+    int max = 0;
+    
+    for (int i=0;i<answer_cnt.size();++i) {
+        if (max<answer_cnt[i]) max = answer_cnt[i];
+    }
+    for (int i=0;i<answer_cnt.size();++i) {
+        if (max == answer_cnt[i]) answer.push_back(i+1);
+    }
+    
     return answer;
 }
