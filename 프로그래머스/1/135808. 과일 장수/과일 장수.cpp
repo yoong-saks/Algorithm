10a29a0866580f21b333906702addd62ec97cc3c
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-03-17 21:42:35 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    sort(score.rbegin(), score.rend());
+    for(int i = m-1; i < score.size(); i+=m) {
+        answer += (m * score[i]);
+    }
+
+    return answer;
+}
