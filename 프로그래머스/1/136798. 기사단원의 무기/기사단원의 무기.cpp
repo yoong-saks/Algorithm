9b9ec1edc0b92624c730fa8284603533eb2de5de
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-03-19 01:53:08 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.07 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int number, int limit, int power) {
+    int answer = 0;
+    for(int i = 1; i <= number; i++) {
+        int cnt = 0;
+        for(int j = 1; j <= sqrt(i); j++) {
+            if(i%j == 0) {
+                ++cnt;
+                if(j != i/j) ++cnt;
+            }
+        }
+        if(cnt > limit) cnt = power;
+        answer += cnt;
+    }
+    return answer;
+}
