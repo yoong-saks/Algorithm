43a47a4b9751800f642e75c5bdac4e3b67daa156
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-03-19 14:14:22 +0900

    [level 1] Title: 소수 찾기, Time: 4.78 ms, Memory: 4.69 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+bool arr[1000001];
+int solution(int n) {
+    int answer = 0;
+    memset(arr, true, sizeof(arr));
+    
+    for(int i = 2; i < sizeof(arr); ++i) {
+        if (!arr[i]) continue;
+        for (int j = i*2; j <= sizeof(arr); j += i) {
+            arr[j] = false;
+        }
+    }
+    
+    for(int i = 2; i <= n; ++i) {
+        if(arr[i] ) answer++;
+    }
+    return answer;
+}
