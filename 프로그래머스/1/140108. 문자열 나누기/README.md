33402810ed92a78585e82000b8185c18a632c682
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-03-21 00:26:47 +0900

    [level 1] Title: 문자열 나누기, Time: 0.05 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/README.md
+++ 프로그래머스/1/140108. 문자열 나누기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 문자열 나누기 - 140108 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 23:48:51
+2024년 03월 21일 00:26:45
 
 ### 문제 설명
 
--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -1,9 +1,25 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
+    char temp = s[0];
+    int sum = 0;
+    int x = 0;
+    int y = 0;
+    for(char c : s) {
+        if(temp == c) x++;
+        else y++;
+        if(x==y) {
+            answer++;
+            sum += (x+y);
+            temp = s[sum];
+            x = y = 0;
+        }
+    }
+    if(x!=y) answer++;
     return answer;
 }
