8f21546789085c71a9cdf8bc5ecb292ae53125cc
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2024-03-28 22:28:09 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.03 ms
+메모리: 3.64 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 28일 20:22:05
+2024년 03월 28일 22:28:07
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -14,33 +14,26 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     }
     for (auto i : reserve) {  //여분을 들고 온 친구들의 옷 개수를 1 늘린다.
         clothes[i-1]++;
-        if(clothes[i-1] == 1) answer++;
+        if(clothes[i-1] == 1) answer++; //만약 여분을 들고 온 학생의 옷 개수가 1개이면, 빌려주지 못하므로, answer 1추가
     }
-    for (int i = 0; i < clothes.size(); ++i) {
-        cout << clothes[i];
-    }
-    cout << endl;
 
     for (int i = 0; i < n; ++i) {
         if(clothes[i] == 0) { //만약 옷이 없는 친구라면 앞과 뒤 친구를 조사해보자
             if(clothes[i-1] == 2) {
-                if(i != 0) {
+                if(i != 0) { // 첫 번째 학생은 예외 처리
                     clothes[i-1]--;
                     clothes[i]++;
                     answer++;
                 }
             }
             else if(clothes[i+1] == 2) {
-                if(i != n-1) {
+                if(i != n-1) { //마지막 학생은 예외 처리
                     clothes[i+1]--;
                     clothes[i]++;
                     answer++;
                 }
             }
         }
-    }
-    for (int i = 0; i < clothes.size(); ++i) {
-        cout << clothes[i];
     }
     return answer;
 }
