7b9ae254c8be2469f13825adac3f3a6e7c8adaac
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-04-13 10:00:45 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,2 +1,3 @@
-SELECT ANIMAL_TYPE, IFNULL(NAME,'No name') NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS;
+select animal_type, ifnull(name, 'No name') as 'name', sex_upon_intake
+from animal_ins
+order by animal_id
