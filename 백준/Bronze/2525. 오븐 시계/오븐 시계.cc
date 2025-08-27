c5b233f52b1043c03bd14a02a01813a8645b97b7
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-08-28 03:51:59 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -1,52 +1,22 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-pair<int, int> numtopair(int n) {
-	int hour = 0;
-	int min = 0;
-	while (1) {
-		if (n >= 60) {
-			hour += 1;
-			n = n - 60;
-		}
-		else {
-			min = n;
-			break;
-		}
-	}
-	pair<int, int> t = make_pair(hour, min);
+int main() {
+    int a, b, c;
 
-	return t;
-}
+    cin >> a >> b >> c;
 
-int main()
-{
-	pair<int, int> clock;
-	cin >> clock.first >> clock.second;
+    a += c / 60;
+    b += c % 60;
 
-	int number;
-	cin >> number;
+    if (b > 59) {
+        a += 1;
+        b -= 60;
+    }
+    if (a > 23) {
+        a -= 24;
+    }
 
-	pair<int, int> plustime = numtopair(number);
-
-	clock.first = clock.first + plustime.first;
-	clock.second = clock.second + plustime.second;
-
-	int plus_h = 0;
-	while (clock.second >= 60) {
-		if (clock.second >= 60) {
-			clock.second %= 60;
-			plus_h += 1;
-		}
-	}
-	
-	clock.first += plus_h;
-
-	if (clock.first >= 24) {
-		clock.first %= 24;
-	}
-
-	cout << clock.first << " " << clock.second << endl;
+    cout << a << " " << b << endl;
 }
