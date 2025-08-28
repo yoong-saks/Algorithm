769d7c06ffb44f05f840b3933706d6afa7fcc9ca
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-08-28 19:42:16 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    while(1) {
+        cin >> a >> b;
+
+        if(!a && !b == 1) {
+            break;
+        }
+        cout << a + b << '\n';
+    }
+}
