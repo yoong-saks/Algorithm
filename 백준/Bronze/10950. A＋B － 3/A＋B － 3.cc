ce3cc71d8d191a033fb1f9da2a401fc6d83ab6df
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-08-28 14:07:57 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main() {
-    int a, b, c;
+    int a;
 
     cin >> a;
 
-    for (int i = 0; i < a; i++) {
+    for (int i, b, c = 0; i < a; i++) {
         cin >> b >> c;
 
         cout << b + c << endl;
