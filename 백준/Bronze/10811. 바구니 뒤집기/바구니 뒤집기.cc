2283ce99f6386cbe6b83b5c26d751bf0e555440c
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-09-01 04:41:17 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int a, b;
+
+    cin >> a >> b;
+
+    vector<int> v(a);
+    iota(v.begin(), v.end(), 1);
+
+    while(b--) {
+        int x, y;
+
+        cin >> x >> y;
+
+        reverse(v.begin() + x - 1, v.begin() + y);
+    }
+
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i] << ' ';
+    }
+    cout << '\n';
+}
