1cb8fe8fbc61a55b973de82d33beee50cf77c06a
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-09-02 05:12:25 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    char c;
+
+    cin >> c;
+    cout << (int)c << '\n';
+
+}
