a616c042cda0a90d820e147508087ab340bb512c
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-09-02 04:59:39 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string s;
+
+    cin >> s;
+
+    cout << s.size() << '\n';
+
+}
