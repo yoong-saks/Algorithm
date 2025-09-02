b7e6aa6d6e92b2fabe0c70dbfe21c3fd08b3ab68
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-09-02 18:44:58 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    vector<int> v(26);
+    string s;
+    int sum = 0;
+
+    fill(v.begin(), v.end(), -1);
+
+    cin >> s;
+
+    for(int i = 0; i < s.size(); i++) {
+        if(v[s[i] - 'a'] == -1) {
+            v[s[i] - 'a'] = i;
+        }
+    }
+
+    for(int i : v) {
+        cout << i << ' ';
+    }
+    cout << '\n';
+}
