b720098954269b47a56f3768866d94a06978bdb8
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-09-04 12:23:17 +0900

    [Bronze II] Title: 단어의 개수, Time: 4 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string s1;
+    int ans = 1;
+
+    getline(cin, s1);
+
+    for(char c : s1) {
+        if(c == ' ') {
+            ans++;
+        }
+    }
+    if(s1[0] == ' ') ans--;
+    if(s1[s1.size() - 1] == ' ') ans--;
+
+    cout << ans << '\n';
+}
