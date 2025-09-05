c291810e9569b9c9e5ea0a0ea560712bdbbb6c07
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-09-05 12:35:55 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cout << "         ,r'\"7" << '\n';
+    cout << "r`-_   ,'  ,/" << '\n';
+    cout << " \\. \". L_r'" << '\n';
+    cout << "   `~\\/" << '\n';
+    cout << "      |" << '\n';
+    cout << "      |" << '\n';
+
+}
