8a0b1de46598d7de056c46dbf9b735a22c0c0f2b
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-09-12 06:20:38 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    vector<vector<int>> v(9,vector<int>(9));
+    int max = -1;
+    int a;
+    int xidx, yidx;
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            cin >> a;
+            if(a > max) {
+                max = a;
+                xidx = i + 1;
+                yidx = j + 1;
+            }
+        }
+    }
+    
+    cout << max << '\n' << xidx << ' ' << yidx << '\n';
+}
