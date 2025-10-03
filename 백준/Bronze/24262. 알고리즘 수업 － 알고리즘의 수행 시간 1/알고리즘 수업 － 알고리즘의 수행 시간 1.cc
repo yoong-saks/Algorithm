327963ee7dd6d989fb94fbe3150f9ec2b3010ef6
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-10-04 08:02:21 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int a;
+
+    cin >> a;
+
+    cout << 1 << '\n';
+    cout << 0 << '\n';
+
+}
