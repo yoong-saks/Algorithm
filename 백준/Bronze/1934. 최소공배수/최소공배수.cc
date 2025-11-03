0e45e81512c8ae7c24157b71b16096955b02ddac
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-11-03 16:20:00 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    int x = max(a, b);
+    int y = min(a, b);
+
+    while(y != 0) {
+        int tmp = x % y;
+        x = y;
+        y = tmp;
+    }
+
+    return x;
+}
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int a;
+    
+    cin >> a;
+
+    for(int i = 0 ; i < a; i++) {
+        int A, B;
+
+        cin >> A >> B;
+
+        cout << A * B / gcd(A, B) << '\n';
+    }
+
+}
