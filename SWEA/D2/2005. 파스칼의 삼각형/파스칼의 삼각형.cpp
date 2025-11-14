765b457a26afaf2111449e510516d24cd049a9e8
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-11-14 14:36:53 +0900

    [D2] Title: 파스칼의 삼각형, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.cpp
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.cpp
@@ -5,35 +5,33 @@ using namespace std;
 
 int dp[11][11];
 
-int get_pascal(int x, int y) {
-    if(y == 1 || x == y) {
+int get_pascal(int i, int j) {
+    if(j == 1 || j == i) {
         return 1;
     }
-
-    if(dp[x][y]) {
-        return dp[x][y];
+    if(dp[i][j]) {
+        return dp[i][j];
     }
 
-    dp[x][y] = get_pascal(x - 1, y - 1) + get_pascal(x - 1, y);
+    dp[i][j] = get_pascal(i - 1, j - 1) + get_pascal(i - 1, j);
 
-    return dp[x][y];
+    return dp[i][j];
 }
-
 int main() {
     int T;
 
     cin >> T;
 
     for(int tc = 1; tc <= T; ++tc) {
-        int N;
+        int n;
 
-        cin >> N;
+        cin >> n;
 
         memset(dp, 0, sizeof(dp));
-
+        
         cout << '#' << tc << '\n';
 
-        for(int i = 1; i <= N; ++i) {
+        for(int i = 1; i <= n; ++i) {
             for(int j = 1; j <= i; ++j) {
                 cout << get_pascal(i, j) << ' ';
             }
