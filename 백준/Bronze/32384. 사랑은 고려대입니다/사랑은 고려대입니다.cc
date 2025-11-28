28ecb3b5c4390c533d57b1eee970729c7d00ce4b
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2025-11-28 10:13:47 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int N;
+    string s = "LoveisKoreaUniversity";
+
+    cin >> N;
+
+    while(N--) {
+        cout << s << ' ';
+    }
+
+    cout << endl;
+
+    return 0;
+}
