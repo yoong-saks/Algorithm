62e9007af2c6b20d76378489f1278e74bc4d80f6
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-02-15 04:55:25 +0900

    [D3] Title: N-Queen, Time: 103 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,48 +1,40 @@
 import java.util.Scanner;
 
 public class Solution {
-	private static int ans, N;
-	
-	private static boolean[] col;
-	private static boolean[] s;
-	private static boolean[] bs;
-	
-//	private int colB;
-//	private int sB;
-//	private int bsB;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		
-		for(int t = 1; t <= T; ++t) {
-			N = sc.nextInt();
-			
-			ans = 0;
-			col = new boolean[N + 1];
-			s = new boolean[2 * N];
-			bs = new boolean[2 * N];
-			
-			dfs(1);
-			System.out.println("#" + t + " " + ans);
-		}
-	}
-	
-	private static void dfs(int row) {
-		if(row > N) {
-			ans++;
-			return;
-		}
-		
-		for(int c = 1; c <= N; ++c) {
-			//유망성 체크
-			if(col[c] || s[row + c - 1] || bs[row - c + N]) continue;
-			
-			col[c] = s[row + c - 1] = bs[row - c + N] = true;
-			dfs(row + 1);
-			col[c] = s[row + c - 1] = bs[row - c + N] = false;
-		}
-		
-	}
+    private static boolean[] col, diag, rdiag;
+    private static int N, ans;
+    // NQueen
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int t = 1; t <= T; ++t) {
+            N = sc.nextInt();
+
+            col = new boolean[N];
+            diag = new boolean[2 * N - 1];
+            rdiag = new boolean[2 * N - 1];
+            ans = 0;
+            btk(0);
+
+            System.out.println("#" + t + " " + ans);
+        }
+    }
+
+    private static void btk(int cnt) {
+        if(cnt == N) {
+            ans++;
+        }
+
+        for(int i = 0; i < N; ++i) {
+            if(col[i]) continue;
+            if(diag[i + cnt]) continue;
+            if(rdiag[N + cnt - i - 1]) continue;
+
+            col[i] = diag[i + cnt] = rdiag[N + cnt - i - 1] = true;
+            btk(cnt + 1);
+            col[i] = diag[i + cnt] = rdiag[N + cnt - i - 1] = false;
+        }
+    }
 }
