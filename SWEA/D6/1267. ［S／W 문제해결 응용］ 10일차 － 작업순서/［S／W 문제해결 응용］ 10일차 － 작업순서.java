d363096ec6cd55d7d177bc0099f5eea0c042b62a
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-02-20 15:37:02 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 107 ms, Memory: 28,672 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -7,78 +8,53 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		// System.setIn(new FileInputStream("res/input.txt"));
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb= new StringBuilder();
-		
+		StringBuilder sb = new StringBuilder();
 		for(int t = 1; t <= 10; ++t) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
+			sb.append("#").append(t);
+			
 			int V = Integer.parseInt(st.nextToken());
 			int E = Integer.parseInt(st.nextToken());
 			
-			st = new StringTokenizer(br.readLine());
-			
-			ArrayList<Integer>[] adjList1 = new ArrayList[V]; // 하향
-			ArrayList<Integer>[] adjList2 = new ArrayList[V]; // 상향
+			ArrayList<Integer>[] graph = new ArrayList[V];
 			
 			for(int i = 0; i < V; ++i) {
-				adjList1[i] = new ArrayList<>();
-				adjList2[i] = new ArrayList<>();
+				graph[i] = new ArrayList<>();
 			}
 			
+			int[] inDegree = new int[V];
+			
+			st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < E; ++i) {
-				int N = Integer.parseInt(st.nextToken()) - 1;
-				int M = Integer.parseInt(st.nextToken()) - 1;
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
 				
-				adjList1[N].add(M);
-				adjList2[M].add(N);
+				graph[start - 1].add(end - 1);
+				
+				inDegree[end - 1]++;
 			}
 			
 			Queue<Integer> q = new ArrayDeque<>();
-			boolean[] completed = new boolean[V];
 			
-			sb.append("#" + t);
-			
-			for(int i = V - 1; i >= 0; --i) {
-				if(adjList2[i].size() == 0) { // 부모가 없는게 시작 노드
-					q.offer(i);
-					
-//					completed[i] = true;
-				}
+			for(int i = 0; i < V; ++i) {
+				if(inDegree[i] == 0) q.offer(i);
 			}
 			
 			while(!q.isEmpty()) {
 				int cur = q.poll();
-
-				if(completed[cur]) continue;
-				boolean flag = false;
+				sb.append(" ").append(cur + 1);
 				
-				for(int i = 0; i < adjList2[cur].size(); ++i) {
-					if(!completed[adjList2[cur].get(i)]) {
-						flag = true;
-						break;
-					}
-				}
-				
-				if(!flag) { // 내 위가 전부 완료됐다면
-					sb.append(" " + (cur + 1));
-					completed[cur] = true;
-					for(int i = 0; i < adjList1[cur].size(); ++i) {
-						if(!completed[adjList1[cur].get(i)]) {
-							q.offer(adjList1[cur].get(i));
-						}
-						
-					}
-				} else { // 내 위가 전부 완료되지 않았다면
-					q.offer(cur); // 다시 넣음
+				for(int i = 0; i < graph[cur].size(); ++i) {
+					if(--inDegree[graph[cur].get(i)] == 0) q.offer(graph[cur].get(i));
 				}
 			}
-			
 			sb.append("\n");
 		}
+		
 		System.out.println(sb);
 	}
 }
