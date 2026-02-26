90b19426ddb19e3c28d4246ae164aa1d1b1bd710
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-02-27 08:55:19 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 180 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		for(int i = 0; i < 4; ++i) {
+			sum += sc.nextInt();
+		}
+		
+		int min = sum / 60;
+		int sec = sum % 60;
+		
+		System.out.println(min + "\n" + sec);
+	}
+
+}
