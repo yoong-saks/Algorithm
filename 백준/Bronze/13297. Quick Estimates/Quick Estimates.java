b675ee39eb7f640d598a0caa5fd7548c05a1c9f2
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-03-04 10:28:58 +0900

    [Bronze IV] Title: Quick Estimates, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N; ++i) {
+			System.out.println(br.readLine().length()); 
+		}
+	}
+
+}
