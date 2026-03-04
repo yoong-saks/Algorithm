71cd785aac79fdb16f9aca92e3c58ac5d11fb17e
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-03-04 15:12:52 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 14488 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
 
@@ -10,12 +11,20 @@ public class Main {
 		
 		String s = br.readLine();
 		
-		int ans = 0;
+		BigInteger ans = BigInteger.valueOf(0);
 		
 		for(int i = 0; i < N; ++i) {
-			ans += ((s.charAt(i) - 'a' + 1) * Math.pow(31, i) % 1234567891);
+			BigInteger num = BigInteger.valueOf((s.charAt(i) - 'a' + 1));
+			BigInteger pow = BigInteger.valueOf(1);
+			for(int j = 0; j < i; ++j) {
+				pow = pow.multiply(BigInteger.valueOf(31));
+			}
+			num = num.multiply(pow);
 			
+			ans = ans.add(num);
 		}
+		
+		ans = ans.mod(BigInteger.valueOf(1234567891));
 		
 		System.out.println(ans);
 	}
