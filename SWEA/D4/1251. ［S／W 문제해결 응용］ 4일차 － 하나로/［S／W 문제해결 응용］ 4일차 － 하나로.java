1f3a6fe724f91782429d2e2e524444efb3cdac9a
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-03-12 13:29:32 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 453 ms, Memory: 105,776 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,94 +1,70 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	private static int[] nodes;
+	private static int[][] islandCoord;
 	
-	private static int find(int n) {
-		if(n == nodes[n]) {
-			return n;
-		}
-		else {
-			return nodes[n] = find(nodes[n]);
-		}
-		
-	}
-	
-	private static void union(int n1, int n2) {
-		int pN1 = find(n1);
-		int pN2 = find(n2);
-		
-		if(pN1 != pN2) nodes[pN1] = pN2;
-	}
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine());
 		
 		for(int t = 1; t <= T; ++t) {
 			int N = Integer.parseInt(br.readLine());
 			
-			int[][] islands = new int[N][2];
-			nodes = new int[N];
+			islandCoord = new int[N][2];
 			
+			st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < N; ++i) {
-				nodes[i] = i;
+				islandCoord[i][0] = Integer.parseInt(st.nextToken());
 			}
-			for(int i = 0; i < 2; ++i) {
-				st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < N; ++i) {
+				islandCoord[i][1] = Integer.parseInt(st.nextToken());
+			}
+			
+			double tax = Double.parseDouble(br.readLine());
+			
+			PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[1], b[1]));
+			boolean[] visited = new boolean[N];
+			
+			pq.add(new long[] {0, 0});
+			long totalDistance = 0;
+			int cnt = 0;
+			
+			while(!pq.isEmpty()) {
+				long[] cur = pq.poll();
 				
-				for(int j = 0; j < N; ++j) {
-					int num = Integer.parseInt(st.nextToken());
+				if(visited[(int)cur[0]]) continue;
+				
+				
+				totalDistance += cur[1];
+				visited[(int)cur[0]] = true;
+				if(++cnt == N) break;
+				
+				for(int i = 0; i < N; ++i) {
+					long d = calculateDistance(islandCoord[(int)cur[0]], islandCoord[i]);
 					
-					islands[j][i] = num;
+					pq.offer(new long[] {i, d});
 				}
 			}
-			
-			double E = Double.parseDouble(br.readLine());
-			
-			PriorityQueue<DSE> distances = new PriorityQueue<>((a, b) -> Long.compare(a.distance, b.distance));
-			for(int i = 0; i < N - 1; ++i) {
-				for(int j = i + 1; j < N; ++j) {
-					long a = Math.abs(islands[i][0] - islands[j][0]);
-					long b = Math.abs(islands[i][1] - islands[j][1]);
-					long distance = a * a + b * b;
-					distances.add(new DSE(distance, i, j));
-				}
-			}
-			
-			long ans = 0;
-			
-			for(int i = 0; i < N - 1; ++i) {
-				DSE cur = distances.poll();
-				if(find(cur.start) == find(cur.end)) {
-					i--;
-					continue;
-				}
-				
-				union(cur.start, cur.end);
-				ans += cur.distance;
-			}
-			
-			System.out.println("#" + t + " " + Math.round(E * ans));
+			sb.append("#").append(t).append(" ").append(Math.round(tax * (totalDistance))).append("\n");
 		}
+		System.out.println(sb);
 	}
-	
-	private static class DSE {
-		long distance;
-		int start;
-		int end;
+	private static long calculateDistance(int[] n1, int[] n2) {
+		long x1 = n1[0];
+		long y1 = n1[1];
+		long x2 = n2[0];
+		long y2 = n2[1];
 		
-		DSE(long distance, int start, int end) {
-			this.distance = distance;
-			this.start = start;
-			this.end = end;
-		}
+		long d1 = Math.abs(x1 - x2);
+		long d2 = Math.abs(y1 - y2);
+		
+		return ((d1 * d1) + (d2* d2));
 	}
-	
 }
