b2fdb73ef8170dcdb9ceabea6bb021efc44d126f
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-04-07 11:41:09 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[1001];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i = 3; i <= 1000; ++i) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10_007;
+		}
+		
+		System.out.println(dp[N]);
+	}
+
+}
