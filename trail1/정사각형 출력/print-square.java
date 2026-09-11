388b694c1ff549dd90c298c535622c6c6ffdddea
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-09-12 03:52:05 +0900

    [Passed] 정사각형 출력 (117ms, 9MB)

---
+++ trail1/정사각형 출력/print-square.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        // ========================================= //
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; ++i) {
+            for(int j = 1; j <= N; ++j) {
+                sb.append(i * N + j).append(" ");
+            }
+            sb.append("\n");
+        }
+        
+        
+        // ========================================= //
+        System.out.println(sb);
+    }
+}
