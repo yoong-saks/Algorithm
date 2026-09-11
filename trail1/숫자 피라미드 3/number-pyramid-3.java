5b20f3116d486362f033d0737579f0befb302b4c
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-09-12 04:39:40 +0900

    [Passed] 숫자 피라미드 3 (90ms, 9MB)

---
+++ trail1/숫자 피라미드 3/number-pyramid-3.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        // ========================================= //
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; ++i) {
+            for(int j = 1; j <= i; ++j) {
+                sb.append(i * j).append(" ");
+            }
+            sb.append("\n");
+        }
+        // ========================================= //
+        System.out.println(sb);
+    }
+}
