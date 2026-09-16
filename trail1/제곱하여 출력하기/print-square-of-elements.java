a24be53cbba78fdeb92cf4c8cea61f44da48f34f
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-09-17 00:14:19 +0900

    [Passed] 제곱하여 출력하기 (93ms, 9MB)

---
+++ trail1/제곱하여 출력하기/print-square-of-elements.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; ++i) {
+            int a = Integer.parseInt(st.nextToken());
+
+            sb.append(a * a).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
