3ec362e759f4e5789adfac9514ccf4c3c4f75fd0
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-09-17 14:51:47 +0900

    [Passed] 대각선으로 정수 채우기 (94ms, 9MB)

---
+++ trail1/대각선으로 정수 채우기/diagonal-numbering.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] matrix = new int[N][M];
+        int cnt = 0;
+
+        for(int k = 0; k < N + M - 1; k++) {
+            for(int j = k; j >= 0; j--) {
+                int i = k - j;
+
+                if (i >= 0 && i < N && j >= 0 && j < M) {
+                    matrix[i][j] = ++cnt;
+                }
+            }
+        }
+
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < M; ++j) {
+                sb.append(matrix[i][j]).append(" ");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
