07649442203b9bbde7314a4db651d23bfce613ce
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-09-19 23:52:38 +0900

    [Passed] 문자열의 길이 출력하기 (89ms, 9MB)

---
+++ trail1/문자열의 길이 출력하기/print-strings-length.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println(br.readLine().length() + br.readLine().length());
+    }
+}
