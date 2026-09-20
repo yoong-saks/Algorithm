73775982d4451a31afcf7a0701709641c51b7b5f
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-09-20 14:03:24 +0900

    [Passed] 문자열 전부 붙이기 (90ms, 9MB)

---
+++ trail1/문자열 전부 붙이기/paste-all-string.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; ++i) {
+            sb.append(br.readLine());
+        }
+
+        System.out.println(sb);
+    }
+}
